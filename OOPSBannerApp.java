f22fb508bc887f6840dbe3b22b9beb63e0908518
Barnamwork <barnamwork@gmail.com>
2026-02-05 20:12:55 +0530

    [Barnam] : Coded UC1 to print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ *
+ * This program prints the literal text "OOPS" to the console.
+ *
+ * @author Barnam
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
