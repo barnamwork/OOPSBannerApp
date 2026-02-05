2ad071ffbe84682fb22ee50c692573c7e1bf1a99
Barnamwork <barnamwork@gmail.com>
2026-02-05 20:18:08 +0530

    [Barnam] : Coded UC2 for printing OOPS in *

---
+++ OOPSBannerApp.java
@@ -0,0 +1,48 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Prints "OOPS" as a banner using '*' and spaces.
+ *
+ * @author Barnam
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(
+                " ******** " + " ******** " + " ******** " + " ******** "
+        );
+
+        // Line 2
+        System.out.println(
+                " *      * " + " *      * " + " *      * " + " *        "
+        );
+
+        // Line 3
+        System.out.println(
+                " *      * " + " *      * " + " *      * " + " *        "
+        );
+
+        // Line 4
+        System.out.println(
+                " *      * " + " *      * " + " ******** " + " ******** "
+        );
+
+        // Line 5
+        System.out.println(
+                " *      * " + " *      * " + " *        " + "        * "
+        );
+
+        // Line 6
+        System.out.println(
+                " *      * " + " *      * " + " *        " + "        * "
+        );
+
+        // Line 7
+        System.out.println(
+                " ******** " + " ******** " + " *        " + " ******** "
+        );
+    }
+}
